8d83bcad2f7658696352ed7d026024a39ada208b
Rik van der Heijden <rik.vanderheijden@zoolt.com>
2024-10-09 16:49:04 +0200

    run number

--- src/cardSection.ts
+++ src/cardSection.ts
@@ -78,7 +78,7 @@ export function createDefaultCardV2Section(): object[] {
       },
       onClick: {
         openLink: {
-          url: `${github.context.serverUrl}/${repoPath}/actions/runs/${github.context.runId}`
+          url: `${github.context.serverUrl}/${repoPath}/actions/runs/${github.context.runNumber}`
         }
       }
     }
